17206721847ae7a21f661ad7828e18f2e1007aaa
darkxaqua <michelle.nguyen.h@gmail.com>
2017-03-26 13:44:24 -0700

    My Django Girls app, first commit

---
+++ python_intro.py
@@ -0,0 +1,23 @@
+print("Hello World");
+
+"""
+if 5>2:
+  print('5 is indeed greater than 2')
+else:
+  print('5 is not greater than 2')
+
+name ='Sonja'
+if name =='Ola':
+  print('Hey Ola')
+elif name == 'Sonja':
+  print('Hey Sonja')
+else:
+  print('Hey Anonymous!')
+
+"""
+def hi():
+  print('Hi there')
+  print('How are you?')
+
+
+hi()
